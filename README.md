9fa93650aff9be4c78cd8d44aa767b9c0e44f9c0
sonke <inwoker123@mail.ru>
2022-11-23 01:48:06 +0300

    new commit allure

---
+++ README.md
@@ -0,0 +1,3 @@
+# Patterns
+[![Build status](https://ci.appveyor.com/api/projects/status/37d4wiuw98603ipp?svg=true)](https://ci.appveyor.com/project/SonKe1/homeworkpatterns2)
+
---
+++ src/test/java/ru/netology/RequestsCardDeliveryTest.java
@@ -0,0 +1,65 @@
+package ru.netology;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.byText;
+
+import java.time.Duration;
+
+
+public class RequestsCardDeliveryTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+
+    }
+
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:9999/");
+    }
+
+    @Test
+    public void shouldCardDelivery() {
+        Information user = Generation.generateUser();
+
+        $("[data-test-id='city'] input").val(user.getCity());
+        $("[data-test-id='date'] input[class='input__control']").
+                sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input[class='input__control']").setValue(Generation.generateDate(5));
+        $("[data-test-id='name'] input").val(user.getName());
+        $("[data-test-id='phone'] input").val(user.getPhone());
+        $x(".//label[@data-test-id='agreement']").click();
+        $(byText("Запланировать")).click();
+
+        $("[data-test-id='success-notification'] div[class='notification__content']").
+                shouldBe(visible, Duration.ofSeconds(15)).should(text("Встреча успешно запланирована на " +
+                        Generation.generateDate(5)));
+        $(byText("Запланировать")).click();
+        $("[data-test-id='date'] input[class='input__control']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input[class='input__control']").sendKeys(Keys.DELETE);
+        $("[data-test-id='date'] input[class='input__control']").setValue(Generation.generateDate(8));
+        $(byText("Запланировать")).click();
+        $("[data-test-id='replan-notification']").shouldBe(visible, Duration.ofSeconds(15));
+        $(byText("Перепланировать")).click();
+        $("[data-test-id='success-notification'] div[class='notification__content']").
+                shouldBe(visible, Duration.ofSeconds(15)).should(text("Встреча успешно запланирована на " +
+                        Generation.generateDate(8)));
+    }
+
+}
